b586a1d7c62705d1337373ff12919ba0cdcbf597
yoshikiito <yoshikiito.el@gmail.com>
2018-05-21 00:19:54 +0900

    agc024追加

---
+++ src/main/agc024/ProblemA.java
@@ -0,0 +1,21 @@
+package agc024;
+
+import java.util.Scanner;
+
+public class ProblemA {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        long k = sc.nextLong();
+
+        if (k % 2 == 1) {
+            System.out.println(b - a);
+        } else {
+            System.out.println(a - b);
+        }
+    }
+}
